64391337080aa72c9fe178d67aa6311d3f168f36
Zake Lee <oscilea@gmail.com>
2021-11-20 00:14:49 +0000

    #40 - update: 편지작성 도메인 디자인 수정

--- src/views/mail/Post.vue
+++ src/views/mail/Post.vue
@@ -1,32 +1,24 @@
 <template>
 	<div class="page-wrapper">
 		<div class="mail-header">
-      <table>
-        <tr>
-          <td>보내는 사람</td>
-          <td>
-            <input placeholder="보내는 사람의 이름을 적어주세요" />
-          </td>
-        </tr>
-        <tr>
-          <td>관계</td>
-          <td>
-            <input placeholder="훈련병과의 관계를 적어주세요" />
-          </td>
-        </tr>
-        <tr>
-          <td>주소</td>
-          <td>
-            <input placeholder="답장을 받을 주소를 입력해주세요" />
-          </td>
-        </tr>
-      </table>  
+      <div class="mail-header-row">
+        <span class="mail-header-row__label font-mobile__content-title me-3">보내는 사람</span>
+        <input class="mail-header-row__input" placeholder="보내는 사람의 이름을 적어주세요" />
+      </div>
+      <div class="mail-header-row">
+        <span class="mail-header-row__label font-mobile__content-title me-3">관계</span>
+        <input class="mail-header-row__input" placeholder="훈련병과의 관계를 적어주세요" />
+      </div>
+      <div class="mail-header-row">
+        <span class="mail-header-row__label font-mobile__content-title me-3">주소</span>
+        <input class="mail-header-row__input" placeholder="답장을 받을 주소를 입력해주세요" />
+      </div>
     </div>
     
     <div class="mail-content masked-overflow">
       <div class="mail-content__input font-mobile__semi-title" placeholder="제목을 입력해주세요" contenteditable></div>
-      <div class="mail-content__textarea font-mobile__content-text" placeholder="내용을 입력해주세요" contenteditable></div>
-      <div class="mail-content__scroll-area"></div>
+      <div class="mail-content__textarea font-mobile__content-text" placeholder="내용을 입력해주세요" contenteditable ref="mailContentInput"></div>
+      <div class="mail-content__focus-area" @click="handleFocusContent"></div>
     </div>
     
     <div class="mail-footer">
@@ -38,27 +30,32 @@
 </template>
 
 <script>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
   
 
 
 export default {
-	components: {
-	},
 	setup() {
 		/* Vuex */
 		const store = useStore()
     
 		/* Router */
 		const router = useRouter()
-			
+		
+    /* Refs */
+    const mailContentInput = ref(null)
+    
 		/* Event Handler */
+    const handleFocusContent = () => mailContentInput.value.focus()
 		
 		return {
+      /* Refs */
+      mailContentInput,
 			/* Variables */
 			/* Functions */
+      handleFocusContent
 		}
 	}
 }
@@ -77,8 +74,29 @@ input {
 }
 .mail {
   &-header {
-    height: 124px;
     padding: 16px 32px;
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    
+    &-row {
+      display: flex;
+      flex-direction: row;
+      
+      &__label {
+        min-width: 57px;
+      }
+      &__input {
+        flex: 1;
+        
+        &::placeholder {
+          font-family: 'Spoqa Han Sans Neo', sans-serif;
+          font-size: 12px;
+          line-height: 16px;
+          color: $gray3;
+        }
+      }
+    }
   }
   &-content {
     flex: 1;
@@ -97,7 +115,6 @@ input {
       }
     }
     &__textarea {
-      min-height: 320px;
       outline: none;
       
       &:empty:before {
@@ -105,8 +122,8 @@ input {
         color: $gray3;
       }
     }
-    &__scroll-area {
-      height: 32px;
+    &__focus-area {
+      height: 300px;
     }
   }
   &-footer {
