c47a00d86faeac89186f0b13f7e30bf1f157a3f3
Zake Lee <oscilea@gmail.com>
2021-11-28 10:06:38 +0000

    #51 - update: 카카오톡 공유 기능

--- src/components/Button/ShareToKakaotalkButton.vue
+++ src/components/Button/ShareToKakaotalkButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="button">
+  <button class="button" @click="handleShareToKakaotalk">
     <div class="button-icon-wrapper">
       <img class="button-icon-wrapper__icon" :src="KakaotalkIconSvg" />
     </div>
@@ -8,13 +8,47 @@
 </template>
 
 <script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+  
 import KakaotalkIconSvg from '@/assets/icons/kakaotalk-icon.svg'
   
 export default {
   setup() {
+    /* Vuex */
+    const store = useStore()
+    const shareLink = computed(() => store.getters.shareLink)
+    console.dir(shareLink.value)
+    
+    /* Event Handler */
+    const handleShareToKakaotalk = () => Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '드디어... 입대합니다!',
+        description: '드디어... 입대합니다! 다들 인편 쓰러 와주실거죠..?',
+        imageUrl:
+          'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/acb43e39-3cfc-4e25-9f1f-3a8cf10315f3/%EB%8C%80%EC%A7%80_1-100.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20211128%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20211128T092253Z&X-Amz-Expires=86400&X-Amz-Signature=daa136114146eb79c2f2dfc585a6ff4f8c54e1393486427c8c1b3a86e6e9a65a&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22%25EB%258C%2580%25EC%25A7%2580%25201-100.jpg%22&x-id=GetObject',
+        link: {
+          mobileWebUrl: 'https://zake-dev.github.io/가나다.1610',
+          webUrl: 'https://zake-dev.github.io/가나다.1610',
+        },
+      },
+      buttons: [
+        {
+          title: '인편 쓰러가기',
+          link: {
+            mobileWebUrl: 'https://zake-dev.github.io/가나다.1610',
+            webUrl: 'https://zake-dev.github.io/가나다.1610',
+          },
+        },
+      ]
+    })
+    
     return {
       /* Assets */
-      KakaotalkIconSvg
+      KakaotalkIconSvg,
+      /* Functions */
+      handleShareToKakaotalk
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -8,15 +8,22 @@ import Mail from '@/store/modules/Mail'
 export default createStore({
 	state: {
 		isModalVisible: false,
-		modalContentName: ''
+		modalContentName: '',
+    userKey: ''
 	},
+  getters: {
+    shareLink: (state) => `rooka.kr/${state.userKey}`
+  },
 	mutations: {
 		SET_IS_MODAL_VISIBLE(state, isVisible) {
 			state.isModalVisible = isVisible
 		},
 		SET_MODAL_CONTENT_NAME(state, name) {
 			state.modalContentName = name
-		}
+		},
+    SET_USER_KEY(state, key) {
+      state.userKey = key
+    }
 	},
 	actions: {
 		OPEN_MODAL({ commit }, modalContentName) {
@@ -25,7 +32,10 @@ export default createStore({
 		},
 		CLOSE_MODAL({ commit }) {
 			commit('SET_IS_MODAL_VISIBLE', false)
-		}
+		},
+    LOGIN_USER({ commit }, key) {
+      commit('SET_USER_KEY', key)
+    }
 	},
 	modules: {
 		registerForm: RegisterForm,
