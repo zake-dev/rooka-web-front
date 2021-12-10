3b26462597e39422b812276eb667ace39f9f080b
zake-dev <zake-dev@github.com>
2021-12-10 16:27:24 +0900

    #61 - update: 404페이지 디자인 수정

--- src/views/error/NotFoundPage.vue
+++ src/views/error/NotFoundPage.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="page-wrapper">
-    <div class="text-content">
-      <p class="font__semi-title">
+    <div class="content">
+      <p class="content__title">
         여긴 어디...?<br />
-        나는 누구...? 👁️👄👁️
+        나는 누구...?
+        <span class="content__title content__icon">👁️👄👁️</span>
       </p>
-      <p class="font__content-text mt-2 mb-3">
-        편지함을 찾을 수 없어요...! 혹시 다른 행성의 주소일까요?<br />
-        주소가 맞게 입력됐는지 다시 한 번 확인해주세요!
+      <p class="content__text">
+        편지함을 찾을 수 없어요...! 혹시 다른 행성의 주소인 걸까요? 주소가 맞게
+        입력됐는지 다시 한 번 확인해주세요!
       </p>
+
+      <div class="decorations">
+        <div class="floating-emoji floating-emoji__postbox">📮</div>
+        <div class="floating-emoji floating-emoji__star">⭐</div>
+        <div class="floating-emoji floating-emoji__ringed-planet">🪐</div>
+        <div class="floating-emoji floating-emoji__rocket">🚀</div>
+        <div
+          class="floating-emoji floating-emoji__globe-showing-asia-australia"
+        >
+          🌏
+        </div>
+        <div class="floating-emoji floating-emoji__dizzy">💫</div>
+        <div class="floating-emoji floating-emoji__alien">👽</div>
+      </div>
     </div>
 
-    <img class="background" :src="BackgroundPng" />
-
     <div class="action-buttons">
-      <BaseButton class="button-primary mb-3" @click="handleClickGoHome"
+      <BaseButton class="button-yellow" @click="handleClickGoHome"
         >홈으로 가기</BaseButton
       >
     </div>
@@ -24,7 +37,6 @@
 <script>
 import { useRouter } from 'vue-router'
 
-import BackgroundPng from '@/assets/images/404-background.png'
 import BaseButton from '@/components/Button/BaseButton'
 
 export default {
@@ -39,8 +51,6 @@ export default {
     const handleClickGoHome = () => router.push('/')
 
     return {
-      /* Assets */
-      BackgroundPng,
       /* Functions */
       handleClickGoHome,
     }
@@ -48,27 +58,87 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
+@import '@/scss/global.scss';
 @import '@/scss/_variables.scss';
 
-main {
-  height: 100% !important;
-  overflow: hidden;
+.page-wrapper {
+  height: 100vh;
+  width: 100vw;
+  background: linear-gradient(180deg, #553a03 17.98%, #191919 82.64%);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
-.text-content {
-  margin-top: 116px;
-  padding: 48px 32px;
-}
-.background {
-  height: 100%;
+.content {
+  flex: 1;
+  padding: 92px 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  &__title {
+    @extend .font__semi-title;
+    color: $white;
+  }
+  &__text {
+    @extend .font__content-text;
+    color: $white;
+  }
+  &__icon {
+    letter-spacing: -0.45rem;
+  }
 }
 .action-buttons {
   position: fixed;
-  bottom: 0;
+  bottom: 60px;
   width: 100%;
-  height: 94px;
-  padding: 16px;
-  display: inline-flex;
+  height: 48px;
+  padding: 0 72px;
+  display: flex;
   justify-content: center;
+}
+.decorations {
+  flex: 1;
+  position: relative;
+}
+.floating-emoji {
+  font-size: 45px;
+
+  &__postbox {
+    position: absolute;
+    top: 20px;
+    right: 0;
+  }
+  &__ringed-planet {
+    position: absolute;
+    top: 65px;
+    right: 0;
+  }
+  &__star {
+    position: absolute;
+    top: 37px;
+    left: 0;
+  }
+  &__globe-showing-asia-australia {
+    position: absolute;
+    top: 35%;
+    left: 0;
+  }
+  &__rocket {
+    position: absolute;
+    top: 25%;
+    left: 25%;
+  }
+  &__dizzy {
+    position: absolute;
+    bottom: 15%;
+    right: 20%;
+  }
+  &__alien {
+    position: absolute;
+    bottom: 20px;
+    right: 0;
+  }
 }
 </style>
