605d6cc2d71fa8c1b84b22d9021b5b1bc0490dfc
zake-dev <zake-dev@github.com>
2022-01-09 07:58:20 +0000

    #87 - dev: https settings and migrate webpack5 from webpack4

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   publicPath: '/absproxy/8081',
   devServer: {
-    public: 'http://34.64.165.207/absproxy/8081',
-    sockPath: '/absproxy/8081/sockjs-node',
     port: 8081,
-    disableHostCheck: true,
-    overlay: false,
+    allowedHosts: 'all',
+    static: {
+      publicPath: '/absproxy/8081',
+    },
+    client: {
+      overlay: false,
+      webSocketURL: 'ws://0.0.0.0:80/absproxy/8081',
+    },
   },
 }
