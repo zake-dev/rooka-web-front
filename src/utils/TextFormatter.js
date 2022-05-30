24e9e3879a9b1c7095991a559f2ee9f32a3b4cfa
이정환 <zake-dev@github.com>
2022-05-30 20:42:07 +0900

    Fix nan error #141

--- src/utils/TextFormatter.js
+++ src/utils/TextFormatter.js
@@ -1,10 +1,21 @@
+const stringToDate = str => {
+  const [year, month, date] = str.split('-').map(n => parseInt(n))
+  return new Date(year, month - 1, date, 0, 0, 0)
+}
+
+const objectToDate = obj => {
+  const { year, month, date } = obj
+  return new Date(year, month - 1, date, 0, 0, 0)
+}
+
 export const toKoreanDateString = date => {
+  console.log(date)
   date =
     date instanceof Date
       ? date
       : date instanceof Object
-      ? new Date(Object.values(date))
-      : new Date(date)
+      ? objectToDate(date)
+      : stringToDate(date)
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
